c2a0c2ef41e879ef6c0f1d7814173aa541229f2b
Shahzad Anwar Khan <fiverr.com/sak_engineering>
2023-01-05 00:43:20 +0500

    update: minor updates in test_main.c

--- I2C/I2C_test_main.c
+++ I2C/I2C_test_main.c
@@ -43,18 +43,22 @@ void main(void) {
     Usart_Init();
     printf("init complete\n\r");
     printf("Test I2C\n\r");
-    printf("Sending single byte 0x34");
+    while(1){
+        int i;
+        for(i=0;i<1000;i++){I2C_Delay();} // arbitrary delay
+    printf("Sending single byte 0x34\n\r\n\r");
     I2C_Transmit_Byte(0x34);
     printf("Sending data 0xA5 to slave address 0x55\n\r\n\r");
-    if(I2C_Single_Write(0x55,0xA5)){
+    if(I2C_Single_Write(slave_address,0xA5)){
         printf("slave responded with ack, data sent\n\r\n\r");
     }
     else{
         printf("slave responded with Nack, data was no sent\n\r\n\r");
     }
     printf("Sending 4 bytes of dummy data to slave address 0x55\n\r\n\r");
-    unsigned int bytes_sent=I2C_Multi_Write(0x55,dummy_data,4);
+    unsigned int bytes_sent=I2C_Multi_Write(slave_address,dummy_data,4);
     
     printf("Slave successfully received %d bytes of data\n\r\n\r",bytes_sent);
+    }
     return;
 }
